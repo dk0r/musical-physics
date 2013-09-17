81b68e31be92884f361aad014aaa38a2d289da8f
dk0r <dk0r.public@gmail.com>
2013-09-16 20:34:50 -0400

    test

---
+++ leap-frog/src/leap-frog.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : leap-frog.cpp
+// Author      : dk0r
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "test!" << endl; // prints
+	return 0;
+}
