9b565c517766c4f14c812e08f9d8148961a593cf
dk0r <dk0r.public@gmail.com>
2013-09-21 10:30:55 -0400

    just includes

---
+++ 2D_wave-equation/src/2D_wave-equation.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : 2D_wave-equation.cpp
+// Author      : dk0r
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include "nr3.h"
+#include "gaussj.h"
+#include "ludcmp.h"
+#include <chrono>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+
+}
